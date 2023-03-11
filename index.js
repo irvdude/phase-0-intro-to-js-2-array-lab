6e045fc2baca393ba02c817a12547e2b370dedcf
Irvin Dinkins <irvdude44@gmail.com>
2023-03-11 12:48:10 -0500

    all test passed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    const newCat = [...cats, name];
+    return newCat;
+}
+
+function prependCat(name) {
+    const newCat = [name, ...cats];
+    return newCat;
+}
+
+function removeLastCat(name) {
+    const removedCat = cats.slice(0, -1);
+    return removedCat;
+}
+
+
+
+function removeFirstCat(name) {
+    const removedCat = cats.slice(1);
+    return removedCat;
+}
+
+console.log(removeFirstCat(cats));
